2be9237d3ed2f22f3e050bf223b004fbc8506ec0
MichaelPizarro <michael.pizarro.l@gmail.com>
2022-03-25 18:24:00 -0300

    Se agrego otra clase

---
+++ src/main/java/com/gestion/productos/prueba/OtraPrueba.java
@@ -0,0 +1,28 @@
+package com.gestion.productos.prueba;
+
+public class OtraPrueba {
+	
+	private int uno;
+	private int dos;
+	private int tres;
+	public int getUno() {
+		return uno;
+	}
+	public void setUno(int uno) {
+		this.uno = uno;
+	}
+	public int getDos() {
+		return dos;
+	}
+	public void setDos(int dos) {
+		this.dos = dos;
+	}
+	public int getTres() {
+		return tres;
+	}
+	public void setTres(int tres) {
+		this.tres = tres;
+	}
+	
+	
+}
