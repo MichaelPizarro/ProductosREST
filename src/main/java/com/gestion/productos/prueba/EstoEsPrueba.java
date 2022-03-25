6469b0dceeefc6f9cdafe3d2bcc347acbb173d43
MichaelPizarro <michael.pizarro.l@gmail.com>
2022-03-25 13:15:26 -0300

    Otros cambios

--- src/main/java/com/gestion/productos/prueba/EstoEsPrueba.java
+++ src/main/java/com/gestion/productos/prueba/EstoEsPrueba.java
@@ -5,4 +5,24 @@ public class EstoEsPrueba {
 	private int numero;
 	private String palabra;
 	private float enumeracion;
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getPalabra() {
+		return palabra;
+	}
+	public void setPalabra(String palabra) {
+		this.palabra = palabra;
+	}
+	public float getEnumeracion() {
+		return enumeracion;
+	}
+	public void setEnumeracion(float enumeracion) {
+		this.enumeracion = enumeracion;
+	}
+	
+	
 }
