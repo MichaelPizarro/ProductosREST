db6273202f223cf186a365ae63d070122c390038
MichaelPizarro <michael.pizarro.l@gmail.com>
2022-03-29 13:30:26 -0300

    Se creo clase getters y setters

---
+++ src/main/java/com/gestion/productos/otraprueba/Prueba2.java
@@ -0,0 +1,38 @@
+package com.gestion.productos.otraprueba;
+
+public class Prueba2 {
+
+    private int numero;
+    private String nombre;
+    private boolean siExsite;
+
+    public Prueba2(int numero, String nombre, boolean siExsite) {
+        this.numero = numero;
+        this.nombre = nombre;
+        this.siExsite = siExsite;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isSiExsite() {
+        return siExsite;
+    }
+
+    public void setSiExsite(boolean siExsite) {
+        this.siExsite = siExsite;
+    }
+}
